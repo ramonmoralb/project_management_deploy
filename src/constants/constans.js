a0ed0feafc3159fcee6278fc3f40ff6b8721cd20
ramonmoreno1 <ramonmoreno1@hotmail.es>
2025-06-11 06:02:34 +0200

    pruebas

--- src/constants/constans.js
+++ src/constants/constans.js
@@ -1,3 +1,3 @@
- export const LOCAL_URL_API = 'https://44.207.39.44/';
- //export const LOCAL_URL_API = 'http://localhost:8002/';
+ export const LOCAL_URL_API = 'https://34.237.79.214/';
+ //export const LOCAL_URL_API = 'https://localhost/';
  export const PROD_URL_API = '';
